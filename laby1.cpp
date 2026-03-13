6af75d8ca7a31f399811cfb71278773c9affe9b1
AleMod1209 <290084@student.pwr.edu.pl>
2026-03-13 08:52:14 +0100

    Add files via upload

---
+++ laby1.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void dodajOsobe(string *tabNazwiska,string Nazwisko){
+	for(int i=0; i<10;i++){
+		if (tabNazwiska[i] == ""){
+			tabNazwiska[i] = Nazwisko;
+			break;
+		};
+	};
+};
+
+void ustawObecnosc(string *tabNazwiska, bool *tabObecnosc, string Nazwisko, bool obecnosc){
+	for(int i=0;i<10;i++){
+		if(tabNazwiska[i]==Nazwisko){
+			tabObecnosc[i] = true;
+			break;
+		};
+	};
+};
+
+void DrukujListe(string *tabNazwiska,bool *tabObecnosc){
+	for(int i=0;i<10;i++){
+		cout<<tabNazwiska[i]<<" "<<tabObecnosc[i]<<endl;
+	};
+};
+
+int main(){
+	string tabNazwiska[10];
+	bool tabObecnosc[10]={};
+	string Nazwisko;
+	bool Obecnosc;
+	int wybor;
+	cout<<"masz do wyboru 1:dodać osobe, 2: ustawić jej obecnosc, 3: wydrukować liste obecnosci, 4:zamknąć program"<<endl;
+	cin>>wybor;
+    while(wybor !=4){
+		switch(wybor){
+			case(1):
+				cout<<"podaj nazwisko"<<endl;
+				cin>>Nazwisko;
+				dodajOsobe(tabNazwiska,Nazwisko);
+				break;
+			case(2):
+				cout<<"podaj nazwisko"<<endl;
+				cin>>Nazwisko;
+				cout<<"podaj czy ma być obecna(true) czy nieobecna(false)"<<endl;
+				cin>>Obecnosc;
+				ustawObecnosc(tabNazwiska,tabObecnosc,Nazwisko,Obecnosc);
+				break;
+			case(3):
+				DrukujListe(tabNazwiska,tabObecnosc);
+				break;
+		};
+	};
+	};
