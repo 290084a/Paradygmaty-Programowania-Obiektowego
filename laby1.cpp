30a999901c89cf56aa3a9c4b6546f8ce2e88eeeb
Aleksander Modrzewski <290084@student.pwr.edu.pl>
2026-05-21 23:18:40 +0200

    Dodana klasa InterfejsUzytkownika oraz poprawiony interfejs zeby lepiej wygladal i pokazywal normalnie polskie znaki

--- laby1.cpp
+++ laby1.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <string>
+#include <cstdlib>
+
 using namespace std;
 
 class Student {
@@ -7,6 +9,7 @@ private:
     string nazwisko;
     string imie;
     string indeks;
+
 public:
     void setNazwisko(string n) {
         nazwisko = n;
@@ -14,136 +17,197 @@ public:
     string getNazwisko() {
         return nazwisko;
     }
+    
     void setImie(string n) {
         imie = n;
     }
-    string getImie(){
+    string getImie() {
         return imie;
     }
+    
     void setIndeks(string n) {
         indeks = n;
     }
     string getIndeks() {
         return indeks;
     }
-    void ustawDane(string imie, string nazwisko, string indeks);
+    
+    void ustawDane(string noweImie, string noweNazwisko, string nowyIndeks) {
+        setImie(noweImie);
+        setNazwisko(noweNazwisko);
+        setIndeks(nowyIndeks);
+    }
 };
 
 class listaObecnosci {
 private:
     Student tabStudent[10];
-    bool tabObecnosc[10];
+    bool tabObecnosc[10] = {};
+
 public:
     void dodajOsobe(Student student);
     void ustawObecnosc(string nazwisko, bool obecnosc);
     void drukujListe();
     void usunOsobe(string nazwisko);
+    
+    Student* getTabStudent() {
+        return tabStudent;
+    }
 };
 
-void Student::ustawDane(string imie, string nazwisko, string indeks){
-    setImie(imie);
-    setNazwisko(nazwisko);
-    setIndeks(indeks);
-}
-
-void listaObecnosci::dodajOsobe(Student student){
-    for(int i=0;i<10;i++)
-    {
-        if(tabStudent[i].getNazwisko()=="")
-        {
-            tabStudent[i]=student;
+void listaObecnosci::dodajOsobe(Student student) {
+    for(int i = 0; i < 10; i++) {
+        if(tabStudent[i].getNazwisko() == "") {
+            tabStudent[i] = student;
             return;
         }
     }
+    cout << "Lista jest pelna!" << endl;
 }
 
-void listaObecnosci::usunOsobe(string nazwisko){
-    for(int i=0;i<10;i++){
-            if(tabStudent[i].getNazwisko()==nazwisko){
-                tabStudent[i]=Student();
-                tabObecnosc[i]=false;
+void listaObecnosci::usunOsobe(string nazwisko) {
+    for(int i = 0; i < 10; i++) {
+        if(tabStudent[i].getNazwisko() == nazwisko) {
+            tabStudent[i] = Student();
+            tabObecnosc[i] = false;
+            cout << "Usunieto studenta: " << nazwisko << endl;
+            return;
+        }
+    }
+    cout << "Nie znaleziono studenta o takim nazwisku." << endl;
+}
+
+void listaObecnosci::ustawObecnosc(string nazwisko, bool obecnosc) {
+    for(int i = 0; i < 10; i++) {
+        if(tabStudent[i].getNazwisko() == nazwisko) {
+            tabObecnosc[i] = obecnosc;
+            cout << "Zaktualizowano obecnosc." << endl;
+            return;
+        }
+    }
+    cout << "Nie znaleziono studenta o takim nazwisku." << endl;
+}
+
+void listaObecnosci::drukujListe() {
+    cout << "\n--- LISTA OBECNOSCI ---" << endl;
+    for(int i = 0; i < 10; i++) {
+        if(tabStudent[i].getNazwisko() != "") {
+            cout << tabStudent[i].getImie() << " " 
+                 << tabStudent[i].getNazwisko() << " (Indeks: " 
+                 << tabStudent[i].getIndeks() << ") - Obecnosc: " 
+                 << tabObecnosc[i] << endl;
+        }
+    }
+    cout << "-----------------------" << endl;
+}
+
+class interfejsUzytkownika {
+private:
+    listaObecnosci *wskaznikNaListe;
+    Student *wskaznikNaTablice;
+    
+    listaObecnosci *listaPpo;
+    listaObecnosci *listaAnaliza;
+
+public:
+    interfejsUzytkownika(listaObecnosci *ppo, listaObecnosci *analiza) {
+        listaPpo = ppo;
+        listaAnaliza = analiza;
+        
+        wskaznikNaListe = listaPpo;
+        wskaznikNaTablice = wskaznikNaListe->getTabStudent();
+    }
+
+    void uruchomPetle();
+};
+
+void interfejsUzytkownika::uruchomPetle() {
+    Student student;
+    string nazwisko, imie, indeks;
+    bool Obecnosc;
+    int wybor = 0;
+    int wyborListy = 0;
+
+    cout << "Wybierz na jakiej liscie chcesz operowac (1 - PPO, 2 - Analiza): ";
+    cin >> wyborListy;
+    if (wyborListy == 1) {
+        wskaznikNaListe = listaPpo;
+    } else {
+        wskaznikNaListe = listaAnaliza;
+    }
+    wskaznikNaTablice = wskaznikNaListe->getTabStudent();
+
+    while(wybor != 6) {
+        cout << "\n=== MENU GLOWNE ===" << endl;
+        cout << "1. Dodaj osobe" << endl;
+        cout << "2. Ustaw obecnosc" << endl;
+        cout << "3. Wydrukuj liste" << endl;
+        cout << "4. Zmien liste (PPO/Analiza)" << endl;
+        cout << "5. Usun osobe" << endl;
+        cout << "6. Zamknij program" << endl;
+        cout << "Twoj wybor: ";
+        cin >> wybor;
+        
+        switch(wybor) {
+            case 1:
+                cout << "Podaj imie: ";
+                cin >> imie;
+                cout << "Podaj nazwisko: ";
+                cin >> nazwisko;
+                cout << "Podaj indeks: ";
+                cin >> indeks;
+                
+                student.ustawDane(imie, nazwisko, indeks);
+                wskaznikNaListe->dodajOsobe(student);
                 break;
-            }
+                
+            case 2:
+                cout << "Podaj nazwisko studenta: ";
+                cin >> nazwisko;
+                cout << "Podaj czy ma byc obecna(1) czy nieobecna(0): ";
+                cin >> Obecnosc;
+                
+                wskaznikNaListe->ustawObecnosc(nazwisko, Obecnosc);
+                break;
+                
+            case 3:
+                wskaznikNaListe->drukujListe();
+                break;
+                
+            case 4:
+                cout << "Wybierz na jakiej liscie chcesz operowac (1 - PPO, 2 - Analiza): ";
+                cin >> wyborListy;
+                if (wyborListy == 1) {
+                    wskaznikNaListe = listaPpo;
+                } else {
+                    wskaznikNaListe = listaAnaliza;
+                }
+                wskaznikNaTablice = wskaznikNaListe->getTabStudent();
+                cout << "Zmieniono aktywna liste!" << endl;
+                break;
+                
+            case 5:
+                cout << "Podaj nazwisko studenta do usuniecia: ";
+                cin >> nazwisko;
+                wskaznikNaListe->usunOsobe(nazwisko);
+                break;
+                
+            case 6:
+                cout << "Zamykanie programu..." << endl;
+                break;
+                
+            default:
+                cout << "Nieprawidlowy wybor. Sprobuj ponownie." << endl;
+                break;
         }
+    }
 }
 
-void listaObecnosci::ustawObecnosc(string nazwisko, bool obecnosc){
-	for(int i=0;i<10;i++){
-		if(tabStudent[i].getNazwisko()==nazwisko){
-			tabObecnosc[i] = obecnosc;
-			break;
-		}
-	}
-}
-
-void listaObecnosci::drukujListe(){
-	for(int i=0;i<10;i++){
-        if(tabStudent[i].getNazwisko() != ""){
-            cout<<tabStudent[i].getImie()<<" "<<tabStudent[i].getNazwisko()<<" "<<tabObecnosc[i]<<endl;
-        }
-	}
-}
-
-int main(){
+int main() {
+    system("chcp 65001 > nul");
     listaObecnosci ppo1;
     listaObecnosci analiza1;
-    Student student;
-	string nazwisko;
-    string imie;
-    string indeks;
-	bool Obecnosc;
-	int wybor = 0;
-    int wyborListy =0;
-    listaObecnosci *aktywnaLista;
-
-
-    cout<<"wybierz czy chcesz operować na liście (1)ppo1 czy (2)analiza1";
-    cin>>wyborListy;
-    switch(wyborListy){
-        case(1):
-            aktywnaLista = &ppo1;
-            break;
-        case(2):
-            aktywnaLista = &analiza1;
-            break;
-    }
-    while(wybor !=5){
-    
-    cout<<"masz do wyboru 1:dodać osobe, 2: ustawić jej obecnosc, 3: wydrukować liste obecnosci, 4:wybór listy, 5: zamknij program"<<endl;
-		cin>>wybor;
-		switch(wybor){
-			case(1):
-                cout<<"podaj imie"<<endl;
-				cin>>imie;
-                cout<<"podaj nazwisko"<<endl;
-				cin>>nazwisko;
-				cout<<"podaj indeks"<<endl;
-				cin>>indeks;
-                student.ustawDane(imie,nazwisko,indeks);
-				aktywnaLista->dodajOsobe(student);
-				break;
-			case(2):
-				cout<<"podaj nazwisko studenta"<<endl;
-				cin>>nazwisko;
-				cout<<"podaj czy ma być obecna(1) czy nieobecna(0)"<<endl;
-				cin>>Obecnosc;
-				aktywnaLista->ustawObecnosc(nazwisko,Obecnosc);
-				break;
-			case(3):
-				aktywnaLista->drukujListe();
-				break;
-            case(4):
-                cout<<"wybierz czy chcesz operować na liście (1)ppo1 czy (2)analiza1";
-                cin>>wyborListy;
-                switch(wyborListy){
-                    case(1):
-                        aktywnaLista = &ppo1;
-                        break;
-                    case(2):
-                        aktywnaLista = &analiza1;
-                        break;
-                }
-
-		};
-	};
+    interfejsUzytkownika menu(&ppo1, &analiza1);
+    menu.uruchomPetle();
+    return 0;
 }
