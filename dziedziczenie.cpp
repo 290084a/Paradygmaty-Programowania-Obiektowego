2ae67447d96d938c4228a37cf8f4d46ef1726441
Aleksander Modrzewski <290084@student.pwr.edu.pl>
2026-05-22 15:16:57 +0200

    praca na zajeciach

--- dziedziczenie.cpp
+++ dziedziczenie.cpp
@@ -12,6 +12,9 @@ public:
     string getImie(){
     return imie;
     }
+    virtual void Drukuj(){
+        cout <<"Osoba: "<< imie << endl;
+    }
 };
 
 class Student: public Osoba {
@@ -24,16 +27,40 @@ class Student: public Osoba {
         string getImie(){
             return imie;
         }
+        void Drukuj(){
+        cout <<"Student: "<< imie << endl;
+        }
+
 };
 
-
+class Pracownik: public Osoba {
+    private:
+        string stanowisko;
+    public:
+        void setImie(string noweImie){
+            imie = noweImie;
+        }
+        string getImie(){
+            return imie;
+        }
+        void Drukuj(){
+        cout <<"Pracownik: "<< imie << endl;
+        }
+};
 
 int main()
 {
+    Osoba *tab[10];
+
     Osoba os1;
-    Student student1;
-    os1.setImie("Aleksander");
-    student1.setImie("Michal");
-    cout << student1.getImie() << endl;
-    cout << os1.getImie() << endl;
+    Student st1;
+    Pracownik pc1;
+
+    tab[0]=&os1;
+    tab[1]=&st1;
+    tab[2]=&pc1;
+
+    tab[0]->Drukuj();
+    tab[1]->Drukuj();
+    tab[2]->Drukuj();
 }
